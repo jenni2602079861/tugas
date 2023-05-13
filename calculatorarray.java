769f4bb375bb0d2d92a8f3d841b9f6296d1e80d8
jenni2602079861 <127701953+jenni2602079861@users.noreply.github.com>
2023-05-13 14:17:20 +0700

    Add files via upload

--- calculatorarray.java
+++ calculatorarray.java
@@ -1,44 +1,33 @@
 import java.util.Scanner;
-import javax.xml.transform.Source;
 public class calculatorarray{
-public static void main(String[] args) {
-    Scanner scn=new Scanner(System.in);
-    System.out.println("Kalkulator Sederhana\n=========================================");
-    System.out.println("Pilih operasi dasar aritmatika berikut:\n1. Penjumlahan\n2. Pengurangan");
-    System.out.print("Pilihan Anda: ");
-    int operator,angka1,angka2,angka3,angka4;
-    operator=scn.nextInt();
-
-    System.out.println("Masukkan angka pertama: ");
-    angka1=scn.nextInt();
-    System.out.println("Masukkan angka kedua: ");
-    angka2=scn.nextInt();
-    System.out.println("Masukkan angka ketiga: ");
-    angka3=scn.nextInt();
-    System.out.println("Masukkan angka keempat: ");
-    angka4=scn.nextInt();
+    public static void main(String[] args) {
+        int[]angka=new int[4];
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Kalkulator Sederhana\n=========================================");
+        System.out.println("Pilih nomor operator dasar aritmatika berikut:\n1. Penjumlahan\n2. Pengurangan");
+        System.out.print("Pilihan Anda: ");
+        int operator=scn.nextInt();
+        for(int i=0;i<angka.length;i++){
+            System.out.print("input angka untuk index ke-" +i+ "= ");
+            angka[i]=scn.nextInt();
+        }
+        System.out.println(angka);
     
-    int [] angka={angka1,angka2,angka3,angka4};
-    int total=angka[0];
-  
-    switch (operator){
-    case 1:
-        for (int i = 1; i < angka.length; i++) {
-        total += angka[i];
+        if(operator==1){
+            for (int i=1; i<angka.length; i++) {
+                angka[0] += angka[i];
+        }
+        System.out.println("Hasil penjumlahan: " + angka[0]);
     }
-        System.out.println("Hasil penjumlahan: " + total);
-        break;
-    case 2:
-    for (int i = 1; i < angka.length; i++) {
-        total -= angka[i];}
-    
-        System.out.println("Hasil pengurangan: " + total);
-        break;
-    default:
-    System.out.println("Tidak Valid");
-    System.exit(0);
-
+        else if(operator==2){
+            for (int i=1; i<angka.length; i++) {
+                angka[0] -= angka[i];
+        }
+        System.out.println("Hasil pengurangan: " + angka[0]);
     }
-
-}
-}
+        else{
+            System.out.println("Tidak Valid");
+            System.exit(0);}
+        
+        }
+    }
