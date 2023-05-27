72c2c7d169ed86390cf7a730f09332f1e6f919a7
jenni2602079861 <127701953+jenni2602079861@users.noreply.github.com>
2023-05-27 15:16:25 +0700

    Add files via upload

--- datapasien.java
+++ datapasien.java
@@ -1,136 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
-
-public class datapasien {
-    static ArrayList<Patient> pasien = new ArrayList<>();
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-
-        while (true) {
-            displayMainMenu();
-            int choose = scn.nextInt();
-
-            switch (choose) {
-                case 1:
-                    inputDataPasien(scn);
-                    break;
-                case 2:
-                    viewDataPasien(scn);
-                    break;
-                case 3:
-                    System.out.println("Program Closed");
-                    return;
-                default:
-                    System.out.println("Invalid");
-            }
-        }
-    }
-
-    static void displayMainMenu() {
-        System.out.println("");
-        System.out.println("            Patient's Data Program");
-        System.out.println("            ++++++++++++++++++++++");
-        System.out.println("1. Input New Data");
-        System.out.println("2. View Patient's Data");
-        System.out.println("3. Exit");
-        System.out.print("Choose: ");
-    }
-
-    static void inputDataPasien(Scanner scn) {
-        System.out.print("Input name [3..20]: ");
-        scn.nextLine();
-        String name = scn.nextLine();
-
-        System.out.print("Input age [10..100]: ");
-        int age = scn.nextInt();
-
-        System.out.print("Enter address [5..30]: ");    
-        scn.nextLine();
-        String address = scn.nextLine();
-
-        if (validateData(name, age, address)) {
-            Patient patient = new Patient(name, age, address);
-            pasien.add(patient);
-            System.out.println("Data has been successfully inserted!");
-        } else {
-            System.out.println("Invalid");
-        }
-    }
-
-    static void viewDataPasien(Scanner scn) {
-        if (pasien.isEmpty()) {
-            System.out.println("No Data!");
-            return;
-        }
-
-        System.out.println("No\tName\tAge\tAddress");
-        for (int i = 0; i < pasien.size(); i++) {
-            Patient patient = pasien.get(i);
-            System.out.println((i + 1) + "\t" + patient.getName() + "\t" + patient.getAge() + "\t" + patient.getAddress());
-        }
-
-        displaySortMenu();
-        int choose = scn.nextInt();
-
-        switch (choose) {
-            case 1:
-                sortDataByNameAscending();
-                break;
-            case 2:
-                sortDataByNameDescending();
-                break;
-            case 3:
-                sortDataByAgeAscending();
-                break;
-            case 4:
-                sortDataByAgeDescending();
-                break;
-            case 5:
-                break;
-            default:
-                System.out.println("Invalid choice. Returning to main menu.");
-        }
-    }
-
-    static void displaySortMenu() {
-        System.out.println("\nSort Data:");
-        System.out.println("1. Sort Data by Name Ascending");
-        System.out.println("2. Sort Data by Name Descending");
-        System.out.println("3. Sort Data by Age Ascending");
-        System.out.println("4. Sort Data by Age Descending");
-        System.out.println("5. Back");
-        System.out.print("Choose: ");
-    }
-
-    static void sortDataByNameAscending() {
-        Collections.sort(pasien, Comparator.comparing(Patient::getName));
-        System.out.println("Data sorted by name (ascending) successfully!");
-    }
-
-    static void sortDataByNameDescending() {
-        Collections.sort(pasien, Comparator.comparing(Patient::getName).reversed());
-        System.out.println("Data sorted by name (descending) successfully!");
-    }
-
-    static void sortDataByAgeAscending() {
-        Collections.sort(pasien, Comparator.comparingInt(Patient::getAge));
-        System.out.println("Data sorted by age (ascending) successfully!");
-    }
-
-    static void sortDataByAgeDescending() {
-        Collections.sort(pasien, Comparator.comparingInt(Patient::getAge).reversed());
-        System.out.println("Data sorted by age (descending) successfully!");
-    }
-
-    static boolean validateData(String name, int age, String address) {
-        return name.length() >= 3 && name.length() <= 20 &&
-                age >= 10 && age <= 100 &&
-                address.length() >= 5 && address.length() <= 30;
-    }
-}
 
 class Patient {
     private String name;
@@ -153,5 +26,148 @@ class Patient {
 
     public String getAddress() {
         return address;
+    }
+}
+
+public class datapasien {
+    private static List<Patient> patients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int choose;
+        do {
+             System.out.println("");
+            System.out.println("            Patient's Data Program");
+            System.out.println("            ++++++++++++++++++++++");
+            System.out.println("1. Input New Data");
+            System.out.println("2. View Patient's Data");
+            System.out.println("3. Exit");
+            System.out.print("Choose: ");
+
+            choose = scn.nextInt();
+            scn.nextLine();
+
+            switch (choose) {
+                case 1:
+                    inputNewData(scn);
+                    break;
+                case 2:
+                    viewPatientsData(scn);
+                    break;
+                case 3:
+                System.out.println("Program closed.");
+                System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid");
+            }
+        } while (choose!=3);
+    }
+
+    private static void inputNewData(Scanner scn) {
+        String name;
+        int age;
+        String address;
+
+        do {
+            System.out.print("Input name [3..20]: ");
+            name = scn.nextLine();
+        } while (name.length()<3 || name.length()>20);
+
+        do {
+            System.out.print("Input age [10..100]: ");
+            try {
+                age = scn.nextInt();
+                scn.nextLine();
+            } catch (InputMismatchException e) {
+                age = -1;
+                System.out.print("Input must be numeric!");
+                System.out.println();
+                scn.nextLine();
+            }
+        } while (age < 10 || age > 100);
+
+        do {
+            System.out.print("Input address [5..30]: ");
+            address = scn.nextLine();
+        } while (address.length()<5 || address.length()>30);
+
+        patients.add(new Patient(name, age, address));
+        System.out.println("Data has been successfully inserted!");
+    }
+
+    private static void viewPatientsData(Scanner scn) {
+        if (patients.isEmpty()) {
+            System.out.println("No Data!");
+            return;
+        }
+
+        System.out.println();
+        displayPatientsData();
+
+        int choice;
+        do {
+            System.out.println();
+            System.out.println("1. Sort Data by Name Ascending");
+            System.out.println("2. Sort Data by Name Descending");
+            System.out.println("3. Sort Data by Age Ascending");
+            System.out.println("4. Sort Data by Age Descending");
+            System.out.println("5. Back");
+            System.out.print("Choose: ");
+
+            choice = scn.nextInt();
+            scn.nextLine();
+            System.out.println();
+
+            switch (choice) {
+                case 1:
+                    sortDataByNameAscending();
+                    break;
+                case 2:
+                    sortDataByNameDescending();
+                    break;
+                case 3:
+                    sortDataByAgeAscending();
+                    break;
+                case 4:
+                    sortDataByAgeDescending();
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice!=5);
+    }
+
+    private static void sortDataByNameAscending() {
+        Collections.sort(patients, Comparator.comparing(Patient::getName));
+        displayPatientsData();
+    }
+
+    private static void sortDataByNameDescending() {
+        Collections.sort(patients, Comparator.comparing(Patient::getName).reversed());
+        displayPatientsData();
+    }
+
+    private static void sortDataByAgeAscending() {
+        Collections.sort(patients, Comparator.comparingInt(Patient::getAge));
+        displayPatientsData();
+    }
+
+    private static void sortDataByAgeDescending() {
+        Collections.sort(patients, Comparator.comparingInt(Patient::getAge).reversed());
+        displayPatientsData();
+    }
+
+    private static void displayPatientsData() {
+        System.out.printf("%-5s%-20s%-5s%-30s%n", "No.", "Name", "Age", "Address  ");
+        System.out.println("=======================================");
+
+         for (int i = 0; i < patients.size(); i++) {
+            Patient patient = patients.get(i);
+            System.out.printf("%-5s%-20s%-5d%-30s%n", (i + 1), patient.getName(), patient.getAge(), patient.getAddress());
+        }
     }
 }
